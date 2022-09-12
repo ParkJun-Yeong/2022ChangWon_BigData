676133d65e9e6309674339ab0322bf3feb490d8c
khs <syt06007@naver.com>
2022-09-12 16:44:27 +0900

    0912

---
+++ RMSE.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class RMSELoss(torch.nn.Module):
+    def __init__(self):
+        super(RMSELoss, self).__init__()
+
+    def forward(self, x, y):
+
+        criterion = nn.MSELoss()
+        
+        eps = 1e-6
+        loss = torch.sqrt(criterion(x,y) + eps)
+
+        return loss
--- model.py
+++ model.py
@@ -8,19 +8,30 @@ class LRD(nn.Module): # Linear ReLU Dropout
 
         self.Linear = nn.Linear(in_ftrs, out_ftrs)
         self.relu = nn.ReLU(inplace=True)
-        self.drop = nn.Dropout(p = 0.3)
+        self.drop = nn.Dropout(p = 0.1)
+    
+    def forward(self, x):
+        x = self.Linear(x)
+        x = self.relu(x)
+        x = self.drop(x)
+
+        return x
 
 class TDNN(nn.Module): # Time DNN
     def __init__(self):
         super(TDNN, self).__init__()
 
-        self.Linear1 = LRD(512, 256)
-        self.Linear2 = LRD(256, 128)
-        self.Linear3 = LRD(128, 1)
+        self.Linear1 = LRD(36, 64)
+        self.Linear2 = LRD(64, 32)
+        self.Linear3 = LRD(32, 1)
+        # self.Linear4 = LRD(32, 1)
+
 
     def forward(self, x):
         x = self.Linear1(x)
         x = self.Linear2(x)
         x = self.Linear3(x)
+        # x = self.Linear4(x)
+
                 
         return x
