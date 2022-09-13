1b2d5f489b92f0f770179a93c345cf08f18c5ea2
ParkJun-Yeong <72220216@dankook.ac.kr>
2022-09-13 16:55:47 +0900

    220913: train, model

--- model.py
+++ model.py
@@ -3,22 +3,33 @@ import torch.nn as nn
 import pandas as pd
 
 
-class Encoder(nn.Module):
-    def __init__(self):
-        super(Encoder, self).__init__()
-
-
-    def forward(self):
-    
-class Decoder(nn.Module):
-    def ___init__(self):
-        super(Decoder, self).__init__()
-    
-class ED_Model(nn.Module):
-    def __init__(self):
-        super(ED_Model, self).__init__()
-        
+# class Encoder(nn.Module):
+#     def __init__(self):
+#         super(Encoder, self).__init__()
+#
+#
+#     def forward(self):
+#
+# class Decoder(nn.Module):
+#     def ___init__(self):
+#         super(Decoder, self).__init__()
+#
+# class ED_Model(nn.Module):
+#     def __init__(self):
+#         super(ED_Model, self).__init__()
+#
 
 class LSTM_Model(nn.Module):
-    def __init__(self):
+    def __init__(self, window_size, input_size):
         super(LSTM_Model, self).__init__()
+        self.window_size = window_size
+        self.input_size = input_size            # the number of input features
+        self.lstm = nn.LSTM(input_size=10, hidden_size=30, num_layers=1, batch_first=True)
+        self.fc == nn.Linear(input_size, batch_first=True)
+
+    def forward(self, X):
+        h0 = torch.randn(self.window_size, 1, self.input_size)
+        c0 = torch.randn(self.window_size, 1, self.input_size)
+        y_pred, (h, c) = self.lstm(X, (h0, c0))
+        y_pred = self.fc(y_pred)
+
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from model import LSTM_Model
 from dataset import Data
+import os
 
 from datetime import datetime
 from tqdm import trange
@@ -11,29 +12,55 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print("device: ", device)
 
 
-def train(dataloader, epochs, model, loss_fn, optimizer):
+def train(tr_dataloader, vl_dataloader, epochs, model, loss_fn, optimizer):
     model.train()
-    tr_loss_hist = []
-    vl_loss_hist = []
     writer = SummaryWriter()
 
     for epoch in trange(epochs):
+        tr_loss_hist = 0.0
+        i = 0
+
         print("[ EPOCH ", epoch, " ]")
 
-        for i, (X, y) in enumerate(dataloader):
+        for i, (X, y) in enumerate(tr_dataloader):
             y_pred = model(X)
             loss = loss_fn(y, y_pred)
+            tr_loss_hist += loss.item()
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
+        mean_tr_loss = tr_loss_hist / float(i)
+        writer.add_scalar("Mean train loss per epoch", mean_tr_loss, epoch)
 
+        now = datetime.now()
+        torch.save(model.state_dict(), os.path.join("./saved_model" + now.strftime("%Y-%m-%d-%H-%M") + "-e" + epoch + ".pt"))
+
+        mean_val_loss = validation(vl_dataloader, model, loss_fn)
+        writer.add_scalar("Mean valid loss per epoch", mean_val_loss, epoch)
+
+
+def validation(dataloader, model, loss_fn):
+    model.eval()
+    val_loss_hist = 0.0
+
+    with torch.no_grad():
+        for i, (X, y) in enumerate(dataloader):
+            y_pred = model(X)
+            loss = loss_fn(y, y_pred)
+            val_loss_hist += loss.item()
+
+        val_loss = val_loss_hist / float(i)
+
+    model.train()
+
+    return val_loss
 
 
 if __name__ == '__main__':
     epoch = 100
-    window_size = 7             # batch_size = num_layers of lstm
+    window_size = 7             # batch_size, so a size of single input is (window_size, 1, 10). it is the same concept of 'seq_len' in nlp
     learning_rate = 1e-1
     weight_decay = 2e-5
 
@@ -50,4 +77,4 @@ if __name__ == '__main__':
     test_dataset = Data(mode='test')
     test_dataloader = DataLoader(test_dataset, batch_size=window_size, shuffle=False)
 
-    train(train_dataloader, epoch, model, loss_fn, optimizer)
+    train(train_dataloader, valid_dataloader, epoch, model, loss_fn, optimizer)
